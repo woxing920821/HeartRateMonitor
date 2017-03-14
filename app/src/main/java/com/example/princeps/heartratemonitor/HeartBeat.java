a58040ecf5a2d71e7512d01bdb371be9a3bec73f
cx16528 <princeps@tmp-125-165.essex.ac.uk>
2017-03-14 16:18:29 +0000

    first commit

---
+++ app/src/main/java/com/example/princeps/heartratemonitor/HeartBeat.java
@@ -0,0 +1,28 @@
+package com.example.princeps.heartratemonitor;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by princeps on 2017/2/22.
+ */
+
+public class HeartBeat extends View {
+    //draw the result in the phone screen
+    public HeartBeat(Context context) {
+        super(context);
+    }
+
+    public HeartBeat(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HeartBeat(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public HeartBeat(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
